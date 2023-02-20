4c83d49ffc791181c928b0464656f8dfc10884a9
cursedseraphim <christian.steinparz@gmail.com>
2023-02-20 09:23:36 +0100

    csv reader

--- bikg_app/routers/rdf_reader.py
+++ bikg_app/routers/rdf_reader.py
@@ -1,27 +1,12 @@
-# A new Python file in this FastAPI project to define an endpoint for reading RDF files from a folder and to import functionality from here into the main FastAPI file.
+# A new Python file in this FastAPI project to define an endpoint for reading RDF files from a folder
 # In this new Python file, we can use Python's RDFLib library to read the RDF files from the folder and process the data as needed.
 
 from fastapi import APIRouter
 from rdflib import Graph
+import pandas as pd
 import os
 
 router = APIRouter()
-
-@router.get("/rdf/{folder_path}")
-async def read_rdf_files(folder_path: str):
-    print(f"Reading RDF files from folder: {folder_path}")
-    g = Graph()
-    for filename in os.listdir(folder_path):
-        print(f"Reading file: {filename}")
-        if filename.endswith(".rdf"):
-            file_path = os.path.join(folder_path, filename)
-            print(f"File path: {file_path}")
-            with open(file_path, "rb") as f:
-                print(f"Reading file: {file_path}")
-                g.parse(data=f.read(), format="application/rdf+xml")
-    print(f"Number of triples: {len(g)}")
-    return {"data": g.serialize(format="json-ld").decode("utf-8")}
-
 
 @router.get("/rdf/file/{file_path}")
 async def read_rdf_file(file_path: str):
@@ -30,6 +15,21 @@ async def read_rdf_file(file_path: str):
     with open(file_path, "rb") as f:
         g = Graph().parse(data=f.read(), format="turtle")
     print(f"Number of triples: {len(g)}")
-    return {"data": g.serialize(format="json-ld")}
+    return {"data": g.serialize(format="ttl")}
 
+@router.get("/csv/file/{file_path}")
+async def read_csv_file(file_path: str):
+    file_path = os.path.join("bikg_app/csv", file_path)
+    print('csv being fetched', file_path)
+    df = pd.read_csv(file_path)
+    # return as csv
+    # return {"data": df.to_csv()}
+    return {"data": df.to_dict(orient="records")}
 
+# @router.get("/rdf/file/{file_path}")
+# async def read_rdf_file(file_path: str):
+#     file_path = os.path.join("bikg_app/rdf", file_path)  # Construct the full file path
+#     with open(file_path, "rb") as f:
+#         g = Graph().parse(data=f.read(), format="turtle")
+#         # subsample 
+#     return {"data": g.serialize(format="ttl")}
