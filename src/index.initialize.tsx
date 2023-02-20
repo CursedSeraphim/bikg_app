272da62d368cc18ac539a0402ed2074edfeb3036
cursedseraphim <christian.steinparz@gmail.com>
2023-02-20 10:53:27 +0100

    fix app+visynapp

--- src/index.initialize.tsx
+++ src/index.initialize.tsx
@@ -18,6 +18,11 @@ ReactDOM.render(
                 Navbar
               </Navbar>
             }
+            appShellProps={{
+              styles: (theme) => ({
+                main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+              }),
+            }}
           >
             {/* Hello app_template! */}
             <App />
