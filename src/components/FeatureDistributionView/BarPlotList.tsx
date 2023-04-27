5aa667ba6d4fb1925e74e0b5f5b69fa5be431cf8
cursedseraphim <christian.steinparz@gmail.com>
2023-04-27 15:50:32 +0200

    cleaning, refactoring, wip

--- src/components/FeatureDistributionView/BarPlotList.tsx
+++ src/components/FeatureDistributionView/BarPlotList.tsx
@@ -6,15 +6,15 @@ import { selectCsvData } from '../Store/CsvSlice';
 /**
  * This function takes the list of all features in the CSV and creates a list of JSX elements frmo the BarPlotSample one below the other to which it hands the feature name as a prop.
  * The csvData does not explicitly have a features list, but it is implicitly there in each of the samples.
- * @returns {JSX.Element[]}
+ * @returns {JSX.Element}
  */
-function BarPlotList(): any {
+function BarPlotList(): JSX.Element {
   const csvData = useSelector(selectCsvData);
 
   const features = csvData && csvData[0] ? Object.keys(csvData[0]) : [];
   return (
     <div className="bar-plot-list-container">
-      {features.map((feature, index) => (
+      {features.map((feature) => (
         <BarPlotSample key={feature} feature={feature} />
       ))}
     </div>
