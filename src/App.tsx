574aa64ef3d021d5a0c612f558b5c377a17e1cc8
cursedseraphim <christian.steinparz@gmail.com>
2023-04-27 16:00:05 +0200

    refactor: extract cytoscape from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import cytoscape from 'cytoscape';
-import coseBilkent from 'cytoscape-cose-bilkent';
-import cytoscapeLasso from 'cytoscape-lasso';
-import { setRdfString, selectRdfData, selectCytoData } from './components/Store/RdfSlice';
+import { setRdfString, selectRdfData } from './components/Store/RdfSlice';
 import { setCsvData, selectCsvDataForPlotly } from './components/Store/CsvSlice';
 import { loadNodes } from './components/Store/NodeSlice';
 import { loadEdges } from './components/Store/EdgeSlice';
@@ -11,19 +8,15 @@ import { loadCytoData } from './components/Store/CytoSlice';
 import InteractiveScatterPlot from './components/EmbeddingView/InteractiveScatterPlot';
 import BarPlotList from './components/FeatureDistributionView/BarPlotList';
 import FixedBarPlotList from './components/FeatureDistributionView/FixedBarPlotList';
+import CytoscapeView from './CytoscapeView';
 
 import './styles.css';
 import { fetchOntology, fetchCSVFile, fetchJSONFile } from './api';
-
-cytoscape.use(cytoscapeLasso);
-cytoscape.use(coseBilkent);
 
 export function App() {
   const dispatch = useDispatch();
   const plotlyData = useSelector(selectCsvDataForPlotly);
   const rdfOntology = useSelector(selectRdfData);
-  const [setCytoData] = React.useState(null);
-  const [cy, setCy] = React.useState(null);
 
   // Fetch ontology
   React.useEffect(() => {
@@ -35,21 +28,6 @@ export function App() {
         console.error('Failed to fetch ontology', error);
       });
   }, [dispatch]);
-
-  // Fetch and process RDF ontology
-  React.useEffect(() => {
-    if (rdfOntology) {
-      selectCytoData({ rdf: { rdfString: rdfOntology } })
-        .then((data) => {
-          setCytoData(data);
-        })
-        .catch((error) => {
-          console.error('Failed to generate Cytoscape data:', error);
-        });
-    }
-    // because adding cytodata to the dependency array causes problems with the lasso selection:
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [rdfOntology]);
 
   // Fetch force directed node positions
   React.useEffect(() => {
@@ -96,80 +74,6 @@ export function App() {
       });
   }, [dispatch]);
 
-  React.useEffect(() => {
-    selectCytoData({ rdf: { rdfString: rdfOntology } })
-      .then((data) => {
-        const newCytoData = data;
-        if (cy) {
-          // Update the cytoData elements and layout
-          cy.elements().remove();
-          // create a deep copy of the cytoData
-          cy.add(newCytoData);
-          cy.lassoSelectionEnabled(true);
-          cy.fit();
-          cy.layout({ name: 'cose-bilkent', idealEdgeLength: 100, nodeDimensionsIncludeLabels: true }).run();
-          // could use (event) => if we want to do something with the event
-          cy.on('boxend', () => {
-            // get the selected nodes
-            const selectedNodes = cy.nodes(':selected');
-
-            // get the data of the selected nodes
-            const selectedData = selectedNodes.map((node) => {
-              return node.data();
-            });
-
-            // selectedData is of the shape [{'id': 'node1', 'label': 'Node 1'}, ...]
-            // create a node list of ids
-            const nodeList = selectedData.map((node) => {
-              return node.id;
-            });
-
-            console.log('nodeList', nodeList);
-          });
-        } else {
-          const newCy = cytoscape({
-            container: document.getElementById('cy'), // container to render in
-
-            wheelSensitivity: 0.2,
-
-            elements: newCytoData,
-
-            style: [
-              // the stylesheet for the graph
-              {
-                selector: 'node',
-                style: {
-                  'background-color': '#666',
-                  label: 'data(id)',
-                },
-              },
-
-              {
-                selector: 'edge',
-                style: {
-                  width: 3,
-                  'line-color': '#ccc',
-                  'target-arrow-color': '#ccc',
-                  'target-arrow-shape': 'triangle',
-                  'curve-style': 'bezier',
-                },
-              },
-            ],
-
-            layout: {
-              name: 'cose-bilkent',
-              idealEdgeLength: 100,
-              nodeDimensionsIncludeLabels: true,
-            },
-          });
-          setCy(newCy);
-        }
-      })
-      .catch((error) => {
-        console.error('Failed to generate Cytoscape data:', error);
-      });
-  }, [cy, rdfOntology]);
-
   const handleDataSelected = (selectedData) => {
     console.log('Data selected in App component', selectedData);
   };
@@ -178,7 +82,7 @@ export function App() {
     <div className="container">
       <div className="Extended-Ontology-View">
         Extended Ontology View
-        <div id="cy" />
+        <CytoscapeView rdfOntology={rdfOntology} />
       </div>
       <div className="Feature-Distribution-View">
         Embedding View
---
+++ src/CytoscapeView.tsx
@@ -0,0 +1,109 @@
+import * as React from 'react';
+import cytoscape from 'cytoscape';
+import coseBilkent from 'cytoscape-cose-bilkent';
+import cytoscapeLasso from 'cytoscape-lasso';
+import { selectCytoData } from './components/Store/RdfSlice';
+
+cytoscape.use(cytoscapeLasso);
+cytoscape.use(coseBilkent);
+
+interface CytoscapeViewProps {
+  rdfOntology: string;
+}
+
+function CytoscapeView({ rdfOntology }: CytoscapeViewProps) {
+  const [cy, setCy] = React.useState<cytoscape.Core | null>(null);
+  const [setCytoData] = React.useState(null);
+
+  // Fetch and process RDF ontology
+  React.useEffect(() => {
+    if (rdfOntology) {
+      selectCytoData({ rdf: { rdfString: rdfOntology } })
+        .then((data) => {
+          setCytoData(data);
+        })
+        .catch((error) => {
+          console.error('Failed to generate Cytoscape data:', error);
+        });
+    }
+    // because adding cytodata to the dependency array causes problems with the lasso selection:
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [rdfOntology]);
+
+  React.useEffect(() => {
+    selectCytoData({ rdf: { rdfString: rdfOntology } })
+      .then((data) => {
+        const newCytoData = data;
+        if (cy) {
+          // Update the cytoData elements and layout
+          cy.elements().remove();
+          cy.add(newCytoData);
+          cy.lassoSelectionEnabled(true);
+          cy.fit();
+          cy.layout({ name: 'cose-bilkent', idealEdgeLength: 100, nodeDimensionsIncludeLabels: true }).run();
+          // could use (event) => if we want to do something with the event
+          cy.on('boxend', () => {
+            // get the selected nodes
+            const selectedNodes = cy.nodes(':selected');
+
+            // get the data of the selected nodes
+            const selectedData = selectedNodes.map((node) => {
+              return node.data();
+            });
+
+            // selectedData is of the shape [{'id': 'node1', 'label': 'Node 1'}, ...]
+            // create a node list of ids
+            const nodeList = selectedData.map((node) => {
+              return node.id;
+            });
+
+            console.log('nodeList', nodeList);
+          });
+        } else {
+          const newCy = cytoscape({
+            container: document.getElementById('cy'), // container to render in
+
+            wheelSensitivity: 0.2,
+
+            elements: newCytoData,
+
+            style: [
+              // the stylesheet for the graph
+              {
+                selector: 'node',
+                style: {
+                  'background-color': '#666',
+                  label: 'data(id)',
+                },
+              },
+
+              {
+                selector: 'edge',
+                style: {
+                  width: 3,
+                  'line-color': '#ccc',
+                  'target-arrow-color': '#ccc',
+                  'target-arrow-shape': 'triangle',
+                  'curve-style': 'bezier',
+                },
+              },
+            ],
+
+            layout: {
+              name: 'cose-bilkent',
+              idealEdgeLength: 100,
+              nodeDimensionsIncludeLabels: true,
+            },
+          });
+          setCy(newCy);
+        }
+      })
+      .catch((error) => {
+        console.error('Failed to generate Cytoscape data:', error);
+      });
+  }, [cy, rdfOntology]);
+
+  return <div id="cy" />;
+}
+
+export default CytoscapeView;
