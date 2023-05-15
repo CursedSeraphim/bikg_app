063f8cdf8537b75afa7f7d8aadfa2e0c9b1de9d8
cursedseraphim <christian.steinparz@gmail.com>
2023-05-15 09:38:54 +0200

    fixe slice import in store

--- src/components/Store/Store.ts
+++ src/components/Store/Store.ts
@@ -1,12 +1,15 @@
+// Store.ts
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import rdfReducer from './RdfSlice';
 import ontologyReducer from './OntologySlice';
 import csvReducer from './CsvSlice';
+import cytoReducer from './CytoSlice';
 
 const rootReducer = combineReducers({
   rdf: rdfReducer,
   csv: csvReducer,
   ontology: ontologyReducer,
+  cyto: cytoReducer,
 });
 
 const store = configureStore({
