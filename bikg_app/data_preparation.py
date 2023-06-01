398632ef50b8dce6b1c5e4cb091b97c163f87acc
cursedseraphim <christian.steinparz@gmail.com>
2023-06-01 16:51:01 +0200

    remove precomputed x/y cyto coordinates

--- bikg_app/data_preparation.py
+++ bikg_app/data_preparation.py
@@ -1,26 +1,18 @@
 import json
 from rdflib import Graph
 
-# file_name = "bikg_app/rdf/omics_model.ttl"
-# layout_file_name = "bikg_app/json/omics_model_layout.json"
-# cytoscape_file_name = "bikg_app/json/omics_model_cytoscape.json"
 file_name = "bikg_app/rdf/ex51_filtered.ttl"
-layout_file_name = "bikg_app/json/ex51_layout.json"
 cytoscape_file_name = "bikg_app/json/ex51_cytoscape.json"
-scaling_factor = 1000
 
 def process_nodes_edges_for_cytoscape(file_path):
-    with open(layout_file_name) as f_layout:
-        layout_data = json.load(f_layout)
         
     g = Graph().parse(file_path, format="turtle")
     nodes, edges, processed_nodes = [], [], {}
     for s, p, o in g:
         for node in (s, o):
             if node not in processed_nodes:
-                layout_info = layout_data.get(str(node), [])
-                position = {"x": layout_info[0]*scaling_factor if len(layout_info) > 0 else 0, 
-                            "y": layout_info[1]*scaling_factor if len(layout_info) > 0 else 0}
+                position = {"x": 0, 
+                            "y": 0}
                 nodes.append({"data": {"id": node, "label": node}, "grabbable": True, "locked": False, "position": position})
                 processed_nodes[node] = True
         
