de2da4eb00dbfc438c47f4a26d1080028e486fb8
Michael Pühringer <51900829+puehringer@users.noreply.github.com>
2023-07-05 12:53:36 +0200

    Update __init__.py

--- bikg_app/__init__.py
+++ bikg_app/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from tdp_core.plugin.model import AVisynPlugin, RegHelper
 
-from .settings import AppSettings
+from .settings import AppSettings, get_settings
 
 
 class VisynPlugin(AVisynPlugin):
@@ -10,6 +11,15 @@ class VisynPlugin(AVisynPlugin):
         from .example_router import example_router
 
         app.include_router(example_router, prefix="/api/app")
+
+        @app.on_event("startup")
+        async def startup():
+            # Add the / path at the very end to match all other routes before
+            bundles_dir = get_settings().bundles_dir
+            if bundles_dir:
+                # Mount the bundles directory as static file to enable the frontend (required in single Dockerfile mode)
+                _log.info(f"Mounting bundles dir: {bundles_dir}")
+                app.mount("/", StaticFiles(directory=bundles_dir, html=True), name="bundle")
 
     def register(self, registry: RegHelper):
         pass
