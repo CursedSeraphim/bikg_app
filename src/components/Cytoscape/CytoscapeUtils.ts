9d664a359aa7c57a39d1583065ec80803c93e295
cursedseraphim <christian.steinparz@gmail.com>
2023-08-25 11:22:57 +0200

    update type definition

--- src/components/Cytoscape/CytoscapeUtils.ts
+++ src/components/Cytoscape/CytoscapeUtils.ts
@@ -1,17 +1,13 @@
 // CytoscapeUtils.ts
-import cytoscape, { Core, ElementDefinition } from 'cytoscape';
+import cytoscape, { Core } from 'cytoscape';
 import React from 'react';
 import { CY_LAYOUT } from './constants';
-
-interface CytoData {
-  nodes: ElementDefinition[];
-  edges: ElementDefinition[];
-}
+import { ICytoData } from '../../types';
 
 type SetCyFn = React.Dispatch<React.SetStateAction<Core | null>>;
 type SetLoadingFn = React.Dispatch<React.SetStateAction<boolean>>;
 
-export function createNewCytoscapeInstance(data: CytoData, setCy: SetCyFn, onLoaded: () => void, setLoading: SetLoadingFn): void {
+export function createNewCytoscapeInstance(data: ICytoData, setCy: SetCyFn, onLoaded: () => void, setLoading: SetLoadingFn): void {
   const newCy = cytoscape({
     container: document.getElementById('cy'),
     wheelSensitivity: 0.2,
@@ -30,7 +26,7 @@ export function createNewCytoscapeInstance(data: CytoData, setCy: SetCyFn, onLoa
 
 export function updateCytoscapeInstance(
   cy: Core,
-  data: CytoData,
+  data: ICytoData,
   initialNodePositions: React.MutableRefObject<Map<string, { x: number; y: number }>>,
   onLoaded: () => void,
   setLoading: SetLoadingFn,
