d454730c04a4b8785d7d0b9793540950c0c8650e
cursedseraphim <christian.steinparz@gmail.com>
2023-09-01 08:04:37 +0200

    comments

--- src/components/Treeview/Treeview.tsx
+++ src/components/Treeview/Treeview.tsx
@@ -17,8 +17,10 @@ export default function Treeview() {
 
   // This function will be called when a node is toggled
   const onToggle = (node, toggled) => {
+    // create a copy of the selected types
     let newSelectedTypes = [...selectedTypesRef.current];
 
+    // define function which will remove a node and its children from the list of selected types
     const removeNodeAndChildrenFromList = (n) => {
       newSelectedTypes = newSelectedTypes.filter((type) => type !== n.name.split(' ')[0]);
       if (n.children) {
@@ -27,13 +29,17 @@ export default function Treeview() {
     };
 
     if (toggled) {
+      // if the node is toggled, i.e., selected, add it to the list of selected types
       newSelectedTypes.push(node.name.split(' ')[0]);
     } else {
+      // if the node is not toggled, i.e., not selected, remove it and its children from the list of selected types
       removeNodeAndChildrenFromList(node);
     }
 
+    // update the list of selected types in the redux store using the new list
     dispatch(setSelectedTypes(newSelectedTypes));
 
+    // update the tree data to reflect the toggled state of the node
     if (node.children) {
       setTreeData((oldTreeData) => {
         const newTreeData = _.cloneDeep(oldTreeData);
