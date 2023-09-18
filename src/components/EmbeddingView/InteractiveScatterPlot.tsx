dc0675e2acd64c6af34d7b13752c290dbfffa1e2
cursedseraphim <christian.steinparz@gmail.com>
2023-09-18 07:44:16 +0200

    start reimplementing scatter in recharts

--- src/components/EmbeddingView/InteractiveScatterPlot.tsx
+++ src/components/EmbeddingView/InteractiveScatterPlot.tsx
@@ -1,22 +1,16 @@
+// InteractiveScatterPlot.tsx
 import React, { useEffect, useRef } from 'react';
-import Plotly from 'plotly.js-dist';
-import createPlotlyComponent from 'react-plotly.js/factory';
-import { BarLoader } from 'react-spinners';
 import { useDispatch } from 'react-redux';
+import { BarLoader } from 'react-spinners';
+import { ScatterChart, Scatter, XAxis, YAxis, Tooltip, Cell } from 'recharts';
 import _ from 'lodash';
 import { setSelectedFocusNodes } from '../Store/CombinedSlice';
-import { SPINNER_COLOR } from '../../constants';
 import store from '../Store/Store';
-import { IScatterPlotProps } from '../../types';
-import { getPlotData, plotLayout } from './PlotlyHelpers';
+import { SPINNER_COLOR, SELECTED_EXEMPLAR_NODE_COLOR, UNSELECTED_EXEMPLAR_NODE_COLOR } from '../../constants';
 
-const Plot = createPlotlyComponent(Plotly);
-
-// TODO replace plotly with a faster library that can handle the updates
-function InteractiveScatterPlot({ data }: IScatterPlotProps) {
+function InteractiveScatterPlot({ data }) {
   const dispatch = useDispatch();
   const selectedFocusNodesRef = useRef<string[]>([]);
-  const plotData = getPlotData(selectedFocusNodesRef.current, data);
 
   useEffect(() => {
     const unsubscribe = store.subscribe(() => {
@@ -26,17 +20,15 @@ function InteractiveScatterPlot({ data }: IScatterPlotProps) {
         selectedFocusNodesRef.current = newSelectedFocusNodes;
       }
     });
-
     return () => unsubscribe();
   }, []);
 
-  const handleSelection = (eventData) => {
-    if (eventData?.points) {
-      const selectedPoints = eventData.points.map((point) => data[point.pointIndex]);
-      const selectedNodes = selectedPoints.map((point) => point.text);
-      selectedFocusNodesRef.current = selectedNodes;
-      dispatch(setSelectedFocusNodes(selectedNodes));
-    }
+  const handleMouseUp = (e, payload) => {
+    // TODO: Perform lasso-like logic here to determine selected points
+    // Update Redux store based on the selected points
+    // For demonstration, selecting points with x and y > 50
+    const selectedNodes = data.filter((d) => d.x > 50 && d.y > 50).map((d) => d.text);
+    dispatch(setSelectedFocusNodes(selectedNodes));
   };
 
   if (data.length === 0) {
@@ -45,14 +37,17 @@ function InteractiveScatterPlot({ data }: IScatterPlotProps) {
 
   return (
     <div className="scatter-plot-container">
-      <Plot
-        data={plotData}
-        layout={plotLayout}
-        onSelected={handleSelection}
-        config={{ displayModeBar: false, responsive: true }}
-        useResizeHandler
-        style={{ width: '100%', height: '100%' }}
-      />
+      <ScatterChart width={400} height={300}>
+        <XAxis dataKey="x" type="number" hide />
+        <YAxis dataKey="y" type="number" hide />
+        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        <Scatter data={data} onMouseUp={handleMouseUp}>
+          {data.map((entry) => {
+            const color = selectedFocusNodesRef.current.includes(entry.text) ? SELECTED_EXEMPLAR_NODE_COLOR : UNSELECTED_EXEMPLAR_NODE_COLOR;
+            return <Cell key={entry.text} fill={color} />;
+          })}
+        </Scatter>
+      </ScatterChart>
     </div>
   );
 }
