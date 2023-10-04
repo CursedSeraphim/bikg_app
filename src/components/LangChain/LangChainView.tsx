19fa3fc277171f8f5ef53d86a4fe72f2a075cbd1
cursedseraphim <christian.steinparz@gmail.com>
2023-10-04 08:51:00 +0200

    remove unnecessary comments

--- src/components/LangChain/LangChainView.tsx
+++ src/components/LangChain/LangChainView.tsx
@@ -11,34 +11,29 @@ function LangchainComponent() {
   const [messages, setMessages] = useState([]);
   const [isBotTyping, setIsBotTyping] = useState(false);
   const executorRef = useRef<AgentExecutor | null>(null);
-  // 1. Add a ref to track the chat history container
   const chatHistoryRef = useRef<HTMLDivElement | null>(null);
-  // 2. Add a state to track if the user has scrolled up manually
   const [hasUserScrolled, setHasUserScrolled] = useState(false);
 
+  // code for auto-scrolling behavior
   useEffect(() => {
     const chatHistoryEl = chatHistoryRef.current;
 
     const handleScroll = () => {
-      // Check if the user has scrolled up, i.e., if scrollTop + offsetHeight is less than the scrollHeight.
       if (chatHistoryEl) {
         setHasUserScrolled(chatHistoryEl.scrollTop + chatHistoryEl.offsetHeight < chatHistoryEl.scrollHeight);
       }
     };
 
-    // Automatically scroll down when a new message is added and user has not scrolled up
     if (chatHistoryEl && !hasUserScrolled) {
       chatHistoryEl.scrollTop = chatHistoryEl.scrollHeight;
     }
 
-    // Add an event listener to the chat history container
     chatHistoryEl?.addEventListener('scroll', handleScroll);
 
-    // Cleanup - remove the event listener when component unmounts
     return () => {
       chatHistoryEl?.removeEventListener('scroll', handleScroll);
     };
-  }, [messages, hasUserScrolled]); // Re-run effect when messages change or hasUserScrolled state changes
+  }, [messages, hasUserScrolled]);
 
   const model = useMemo(
     () =>
@@ -82,7 +77,6 @@ function LangchainComponent() {
 
   return (
     <div className="chat-container">
-      {/* 3. Add a ref to the chat history container */}
       <div ref={chatHistoryRef} className="chat-history">
         {messages.map((message) => (
           <div key={message.id} className={`chat-message ${message.isUser ? 'user' : 'bot'}`}>
