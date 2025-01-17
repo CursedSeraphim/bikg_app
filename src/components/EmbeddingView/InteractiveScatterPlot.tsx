4e2e203e2ad2c388f44c81132ce37d3463d3c2f8
cursedseraphim <christian.steinparz@gmail.com>
2025-01-17 13:23:50 +0100

    placeholder selection color in scatterplot

--- src/components/EmbeddingView/InteractiveScatterPlot.tsx
+++ src/components/EmbeddingView/InteractiveScatterPlot.tsx
@@ -111,11 +111,10 @@ function CanvasScatterPlot({ data }: IScatterPlotProps) {
 
     // Draw each point
     data.forEach((d) => {
-      // could be used for different visual encoding based on selection
-      // const isSelected = selectedNodes.includes(d.text);
+      const isSelected = selectedNodes.includes(d.text);
 
       ctx.beginPath();
-      ctx.fillStyle = '#DA5700';
+      ctx.fillStyle = isSelected ? 'orange' : '#DA5700';
       const cx = xScale(d.x) + margins.left;
       const cy = yScale(d.y) + margins.top;
       ctx.arc(cx, cy, 4, 0, 2 * Math.PI);
