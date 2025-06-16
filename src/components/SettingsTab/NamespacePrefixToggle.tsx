669ff06864f4649ff1fcfde82d6ef126039f7daa
Christian Alexander Steinparz <christian.steinparz@gmail.com>
2025-06-16 13:45:07 +0200

    fix namespace toggle display

--- src/components/SettingsTab/NamespacePrefixToggle.tsx
+++ src/components/SettingsTab/NamespacePrefixToggle.tsx
@@ -16,10 +16,18 @@ function NamespacePrefixToggle() {
   return (
     <Group direction="column" spacing="xs">
       <Tooltip label="Hide namespace prefixes from column labels." multiline withArrow w={220} transitionProps={{ duration: 300 }}>
-        <Switch checked={hideColumns} onChange={(e) => dispatch(setHideNamespacePrefixColumns(e.currentTarget.checked))} label="Prefix in Columns" />
+        <Switch
+          checked={!hideColumns}
+          onChange={(e) => dispatch(setHideNamespacePrefixColumns(!e.currentTarget.checked))}
+          label="Prefix in Columns"
+        />
       </Tooltip>
       <Tooltip label="Also hide prefixes in cell values." multiline withArrow w={220} transitionProps={{ duration: 300 }}>
-        <Switch checked={hideCells} onChange={(e) => dispatch(setHideNamespacePrefixCells(e.currentTarget.checked))} label="Prefix in Cells" />
+        <Switch
+          checked={!hideCells}
+          onChange={(e) => dispatch(setHideNamespacePrefixCells(!e.currentTarget.checked))}
+          label="Prefix in Cells"
+        />
       </Tooltip>
     </Group>
   );
