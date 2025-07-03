eb5f05669ad082b358cc3c189bb6e9c0c1254ac0
Christian Alexander Steinparz <christian.steinparz@gmail.com>
2025-07-03 10:07:33 +0200

    Add title element to D3 context menu

--- src/components/D3NodeLink/hooks/useD3ContextMenu.tsx
+++ src/components/D3NodeLink/hooks/useD3ContextMenu.tsx
@@ -108,6 +108,9 @@ export function useD3ContextMenu(
     >
       {state.targetNode ? (
         <>
+          <div className="d3-context-menu-title">
+            {state.targetNode.label}
+          </div>
           <button type="button" className="d3-context-menu-item">
             Node action for {state.targetNode.label}
           </button>
