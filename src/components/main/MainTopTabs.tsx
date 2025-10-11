347028b3f63890efeb7753cd65d65cd91f533338
Christian Alexander Steinparz <christian.steinparz@gmail.com>
2025-10-11 09:54:48 +0200

    Update view labels and simplify top diagram tabs

--- src/components/main/MainTopTabs.tsx
+++ src/components/main/MainTopTabs.tsx
@@ -2,16 +2,14 @@
 import { Tabs, Tooltip } from '@mantine/core';
 import React from 'react';
 import { useSelector } from 'react-redux';
-import CytoscapeView from '../Cytoscape/CytoscapeView';
 import D3ForceGraph from '../D3NodeLink/D3ForceGraph';
 import { selectRdfData } from '../Store/CombinedSlice';
 
 export default function MainTopTabs() {
   const rdfOntology = useSelector(selectRdfData);
-  const [cytoscapeLoading, setCytoscapeLoading] = React.useState(true);
 
   return (
-    <Tabs defaultValue="D3.js NLD View" style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+    <Tabs defaultValue="Node-Link Diagram" style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
       <Tabs.List>
         <Tooltip
           multiline
@@ -19,28 +17,14 @@ export default function MainTopTabs() {
           opacity={0.7}
           withArrow
           transitionProps={{ duration: 300 }}
-          label="Displays the ontology, SHACL constraints, and violation data as a D3 node-link diagram."
+          label="Displays the ontology, SHACL constraints, and violation data as a node-link diagram."
         >
-          <Tabs.Tab value="D3.js NLD View">D3.js NLD View</Tabs.Tab>
-        </Tooltip>
-        <Tooltip
-          multiline
-          w={220}
-          opacity={0.7}
-          withArrow
-          transitionProps={{ duration: 300 }}
-          label="Displays the ontology, SHACL constraints, and violation data as a Cytoscape node-link diagram."
-        >
-          <Tabs.Tab value="Cytoscape NLD View">Cytoscape NLD View</Tabs.Tab>
+          <Tabs.Tab value="Node-Link Diagram">Node-Link Diagram</Tabs.Tab>
         </Tooltip>
       </Tabs.List>
 
-      <Tabs.Panel value="D3.js NLD View" pt="xs" style={{ flex: 1, overflow: 'auto' }}>
+      <Tabs.Panel value="Node-Link Diagram" pt="xs" style={{ flex: 1, overflow: 'auto' }}>
         <D3ForceGraph rdfOntology={rdfOntology} />
-      </Tabs.Panel>
-
-      <Tabs.Panel value="Cytoscape NLD View" pt="xs" style={{ flex: 1, overflow: 'auto' }}>
-        <CytoscapeView rdfOntology={rdfOntology} onLoaded={() => setCytoscapeLoading(false)} />
       </Tabs.Panel>
     </Tabs>
   );
