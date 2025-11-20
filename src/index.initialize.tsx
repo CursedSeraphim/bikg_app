4fcc9ca8c04aef1aed7d68e9ca41ada0e920d34e
cursedseraphim <christian.steinparz@gmail.com>
2025-11-20 09:22:18 +0100

    :art: change header color / matine color constant

--- src/index.initialize.tsx
+++ src/index.initialize.tsx
@@ -8,16 +8,29 @@ import { App } from './App';
 import { AppNameElement } from './components/AppName';
 import { BILogo } from './components/BILogo';
 import store from './components/Store/Store';
+import { MANTINE_HEADER_COLOR } from './constants';
 
 ReactDOM.render(
   // <React.StrictMode> TODO doesn't work with treebeard due to depreacted finddomnode
-  <MantineProvider withGlobalStyles withNormalizeCSS>
+  <MantineProvider
+    withGlobalStyles
+    withNormalizeCSS
+    theme={{
+      primaryColor: 'dark',
+      globalStyles: () => ({
+        ':root': {
+          '--mantine-header-color': MANTINE_HEADER_COLOR,
+        },
+      }),
+    }}
+  >
     <Provider store={store}>
       <VisynAppProvider appName="app_template">
         <VisynApp
           loginMenu={null}
           header={
             <VisynHeader
+              backgroundColor={MANTINE_HEADER_COLOR}
               components={{
                 title: (
                   <Title order={3} weight={100} color="white">
