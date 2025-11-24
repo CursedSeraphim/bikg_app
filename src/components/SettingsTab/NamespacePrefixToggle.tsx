3f3f00bfc1f9b8f55daf3bd84a86233d5c3e4601
cursedseraphim <christian.steinparz@gmail.com>
2025-11-24 11:29:33 +0100

    :art: feat(ui): replace invalid Group direction prop with Stack in NamespacePrefixToggle

--- src/components/SettingsTab/NamespacePrefixToggle.tsx
+++ src/components/SettingsTab/NamespacePrefixToggle.tsx
@@ -1,4 +1,4 @@
-import { Group, Switch, Tooltip } from '@mantine/core';
+import { Stack, Switch, Tooltip } from '@mantine/core';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -14,22 +14,15 @@ function NamespacePrefixToggle() {
   const hideCells = useSelector(selectHideNamespacePrefixCells);
 
   return (
-    <Group direction="column" spacing="xs">
+    <Stack spacing="xs">
       <Tooltip label="Hide namespace prefixes from column labels." multiline withArrow w={220} transitionProps={{ duration: 300 }}>
-        <Switch
-          checked={!hideColumns}
-          onChange={(e) => dispatch(setHideNamespacePrefixColumns(!e.currentTarget.checked))}
-          label="Prefix in Columns"
-        />
+        <Switch checked={!hideColumns} onChange={(e) => dispatch(setHideNamespacePrefixColumns(!e.currentTarget.checked))} label="Prefix in Columns" />
       </Tooltip>
+
       <Tooltip label="Also hide prefixes in cell values." multiline withArrow w={220} transitionProps={{ duration: 300 }}>
-        <Switch
-          checked={!hideCells}
-          onChange={(e) => dispatch(setHideNamespacePrefixCells(!e.currentTarget.checked))}
-          label="Prefix in Cells"
-        />
+        <Switch checked={!hideCells} onChange={(e) => dispatch(setHideNamespacePrefixCells(!e.currentTarget.checked))} label="Prefix in Cells" />
       </Tooltip>
-    </Group>
+    </Stack>
   );
 }
 
